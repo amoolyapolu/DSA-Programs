6e1e8c56c69a3b0ec13e069d64b126ef5a805536
amoolyapolu <96608846+amoolyapolu@users.noreply.github.com>
2024-05-13 00:06:40 +0530

    problem statement

---
+++ Graphs/BFS/find_level_of_k.py
@@ -0,0 +1,6 @@
+"""
+Given an undirected graph with V vertices and E edges and a node X, 
+The task is to find the level of node X in the undirected graph. If X does not exist in the graph then return -1.
+Input: V = 5, Edges = {{0, 1}, {0, 2}, {1, 3}, {2, 4}}, X = 3
+Output: 2
+"""
